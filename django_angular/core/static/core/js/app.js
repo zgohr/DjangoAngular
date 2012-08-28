027deab1cff9f920b9f4ead26c004eb94fcc5c0f
Zach Gohr <zachary.gohr@gmail.com>
2012-08-28 14:09:22 -0500

    Continued work on the order example.

--- django_angular/core/static/core/js/app.js
+++ django_angular/core/static/core/js/app.js
@@ -6,11 +6,11 @@ function TraderCtrl($scope) {
       item: $scope.orderItem,
       amount: $scope.orderAmount,
       quantity: $scope.orderQuantity,
-      value: $scope.orderAmount * $scope.orderQuantity,
+      //value: $scope.orderAmount * $scope.orderQuantity,
       fills: 0,
-      inventory_value: 0,
+      //inventory_value: 0,
       filled: false,
-      cancelled: false
+      complete: false
     });
     $scope.orderItem = '';
     $scope.orderAmount = '';
@@ -18,12 +18,41 @@ function TraderCtrl($scope) {
   };
 
   $scope.deleteOrder = function(order) {
-    $scope.orders.pop(order);
+    $scope.orders.splice($.inArray(order, $scope.orders),1);
+  };
+
+  $scope.markComplete = function(order){
+    order.complete = !order.complete;
+    return order.complete;
+  };
+
+  $scope.orderValue = function(order){
+    var total = 0;
+    if (order) {
+      return Math.abs((order.quantity - order.fills) * order.amount);
+    }
+    angular.forEach($scope.orders, function(order){
+      total += (order.quantity - order.fills) * order.amount;
+    });
+    return Math.abs(total);
+  };
+
+  $scope.getAbsQuantity = function(order){
+    return Math.abs(order.quantity)
+  };
+
+  $scope.inventoryValue = function(order){
+    var total = 0;
+    if (order) {
+      return Math.abs(order.fills * order.amount);
+    }
+    angular.forEach($scope.orders, function(order){
+      total += order.fills * order.amount;
+    });
+    return total;
   };
 
   $scope.fills = function(order) {
-    order.inventory_value = order.fills * order.amount;
-
     if (order.fills === order.quantity) {
       order.filled = true;
     } else {
